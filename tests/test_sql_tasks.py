b87d5ee895fc591d1a8d17e1b14533755e0fcef6
RyszardCetnarskiEY <ryszard.cetnarski@pl.ey.com>
2025-06-23 15:38:58 +0000

    Stable: black + isort formatting only

--- tests/test_sql_tasks.py
+++ tests/test_sql_tasks.py
@@ -29,9 +29,7 @@ def df_and_params():
     parsed_df = pd.read_csv(path, index_col=0)
 
     # todo - perfrom testing on all params, not just 0 - perhaps it is best to sace the test_params to json or pickle
-    test_params = _generate_run_parameters_logic(
-        HISTORICAL_START_DATE, HISTORICAL_END_DATE
-    )
+    test_params = _generate_run_parameters_logic(HISTORICAL_START_DATE, HISTORICAL_END_DATE)
 
     return {"df": parsed_df, "params": test_params[0]}
 
@@ -40,9 +38,10 @@ def df_and_params():
 def staging_dict():
     import pickle
 
-    with open(
-        "/workspace/myairflowdags/tests/data/staging_result_prices_pl.pkl", "rb"
-    ) as f:
+    filename = "staging_result_prices_pl.pkl"
+    path = os.path.join(os.path.dirname(__file__), "data", filename)
+
+    with open(path, "rb") as f:
         result = pickle.load(f)
     return result
 
@@ -78,9 +77,7 @@ def test_create_table_columns(df_and_params, expected_columns):
     df = df_and_params["df"]
     df = df.drop("Position", axis=1)  # Todo, a little ugly but not sure how to fix now
 
-    assert _create_table_columns(df) == expected_columns, repr(
-        _create_table_columns(df)
-    )
+    assert _create_table_columns(df) == expected_columns, repr(_create_table_columns(df))
 
 
 @patch("tasks.sql_tasks.random.randint", return_value=12345)
@@ -101,9 +98,7 @@ def test_load_to_staging_table_with_data(
     assert result["staging_table_name"].startswith(
         expected_table[:63]
     ), f"Expected table name {result['staging_table_name']}"
-    assert (
-        result["var_name"] == "Energy Prices fixing I"
-    ), f"Expected var name {result['var_name']}"
+    assert result["var_name"] == "Energy Prices fixing I", f"Expected var name {result['var_name']}"
     assert hook.run.call_count >= 2, hook.run.call_count
 
     hook.run.assert_any_call(
@@ -131,9 +126,7 @@ def test_merge_executes_sql(mock_pg_hook_class, mock_create_prod_table, staging_
     production_table = "energy_prices_fixing_i"
     staging_table = "stg_entsoe_PL_202501010000_12345"
 
-    assert (
-        result == f"Merged {staging_table}"
-    ), f"expected {staging_table} actual {result}"
+    assert result == f"Merged {staging_table}", f"expected {staging_table} actual {result}"
     mock_create_prod_table.assert_called_once_with(production_table)
 
     assert mock_pg_hook.run.call_count == 1
@@ -156,9 +149,7 @@ def test_create_initial_tables(mock_logger, mock_pg_hook_class):
     assert expected_sql_snippet in actual_sql
     mock_pg_hook.run.assert_called_once()
     assert result == {"raw_xml_table": "test_raw_table"}
-    mock_logger.info.assert_called_once_with(
-        'Ensured raw XML table airflow_data."test_raw_table" exists.'
-    )
+    mock_logger.info.assert_called_once_with('Ensured raw XML table airflow_data."test_raw_table" exists.')
 
 
 @patch("tasks.sql_tasks.PostgresHook")
@@ -174,9 +165,7 @@ def test_create_prod_table(mock_logger, mock_pg_hook_class):
 
     assert expected_sql_snippet in actual_sql
     mock_pg_hook.run.assert_called_once()
-    mock_logger.info.assert_called_once_with(
-        'Ensured production table airflow_data."my_var" exists.'
-    )
+    mock_logger.info.assert_called_once_with('Ensured production table airflow_data."my_var" exists.')
 
 
 @patch("tasks.sql_tasks.PostgresHook")
@@ -188,9 +177,5 @@ def test_cleanup_staging_tables_drops_table(mock_logger, mock_pg_hook_class):
     staging_dict = {"staging_table_name": "stg_table_x"}
     cleanup_staging_tables.function(staging_dict, "fake_conn")
 
-    mock_pg_hook.run.assert_called_once_with(
-        'DROP TABLE IF EXISTS airflow_data."stg_table_x";'
-    )
-    mock_logger.info.assert_called_once_with(
-        'Dropped staging table: airflow_data."stg_table_x".'
-    )
+    mock_pg_hook.run.assert_called_once_with('DROP TABLE IF EXISTS airflow_data."stg_table_x";')
+    mock_logger.info.assert_called_once_with('Dropped staging table: airflow_data."stg_table_x".')
